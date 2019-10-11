06458a9d76f61fbdea119b9741f6ef21509180a2
wangwei3 <986299889@qq.com>
2019-10-11 18:30:08 +0800

    update

--- main/main.go
+++ main/main.go
@@ -1,19 +1,27 @@
 package main
 
 import (
-
+	"fmt"
 	"github.com/CodisLabs/codis/pkg/models/zk"
 	"time"
-	"fmt"
 )
 
 func main() {
+	//s := float32(5.4)
+	//ss := 300 * s
+	//d := float32(160.00)
+	//dd := d / 1240
+	//fmt.Println(dd)
+	testZk()
+}
+
+func testZk() {
 	client, err := zkclient.New("172.16.11.133", "", time.Second)
-	if err != nil{
-             fmt.Print("err")
+	if err != nil {
+		fmt.Print("err")
 	}
 	data, err := client.Read("/redis/raas/cluster/redis:test-w456", true)
-	if err != nil{
+	if err != nil {
 		fmt.Print(err.Error())
 	}
 	fmt.Println(string(data))
